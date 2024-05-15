3e1445a25771e17d094bcc907988268498ae2881
Dan <jacobydan01@gmail.com>
2024-05-15 00:00:04 -0400

    instanceMethod

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,13 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
+#self is always used as parameter for instance method
+#to add a person, Bob:
+#Bob = Person()
+# to make Bob talk:
+#Bob.talk()
